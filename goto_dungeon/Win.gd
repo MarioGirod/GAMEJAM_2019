0be8d42cfc05d48a4c6b8e4df21eca924ad40674
MarioGriod <mario.girod@gmail.com>
2019-12-01 11:25:31 +0100

    nice background colors
    level selection using numbers
    
    messages for each screen

--- goto_dungeon/Win.gd
+++ goto_dungeon/Win.gd
@@ -1,10 +1,13 @@
 extends Node2D
 var start_scene_path = "res://Start.tscn"
+var font = preload("res://assets/RobotoBold120.tres")
 
-func _ready():
-	pass 
+
+func _draw():
+	font.size = 50
+	draw_string(font, Vector2(270,520), "Dungon Quest solved", Color(1,1,1)) 
 
 
 func _input(event):
-	if event.is_action_pressed('ui_focus_next') or event.is_action_pressed('ui_cancel'):
+	if event.is_action_pressed('ui_accept'):
 		globals.switch_scene(start_scene_path)
