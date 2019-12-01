f084a8af7bc939ae5151f640fc48bcde6231a2f9
fxwt <fwollert@gmail.com>
2019-12-01 15:12:10 +0100

    tweak player positions

--- goto_dungeon/Levels/Level1.gd
+++ goto_dungeon/Levels/Level1.gd
@@ -9,7 +9,7 @@ onready var room2 = get_node("Room2")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	globals.current_level_path = "res://Levels/Level1.tscn"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
